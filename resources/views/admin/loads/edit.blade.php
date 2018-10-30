718ef8cda2df914ad1f58ceb8c647884cffaa159
Marsellkin <dizline@live.com>
2018-10-30 10:42:15 +0300

    Update edit.blade.php

--- resources/views/admin/loads/edit.blade.php
+++ resources/views/admin/loads/edit.blade.php
@@ -28,7 +28,7 @@
                 <?php $inputParent = getInput('parent', $load->parent_id); ?>
 
                 <select class="form-control" id="parent" name="parent">
-                    <option value="0">Основной форум</option>
+                    <option value="0">Основной раздел</option>
 
                     @foreach ($loads as $data)
 
